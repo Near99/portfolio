a6fd47779b1901d6e9d95e25cad90627e0622e26
Near99 <wkx0823@gmail.com>
2021-08-19 21:40:25 +0800

    mock navbar style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div>
+    <Router>
+      <Sidebar />
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
