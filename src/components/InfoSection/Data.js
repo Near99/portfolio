d838a8542cf16b65dfe093af1d289f9b38ba7d45
Near99 <wkx0823@gmail.com>
2021-08-21 00:08:40 +0800

    deploy test

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,4 +1,6 @@
-import pic from "../../images/svg-2.svg";
+import infoSectionPic from "../../images/svg-2.svg";
+import infoSectionPic2 from "../../images/svg-1.svg";
+import infoSectionPic3 from "../../images/svg-3.svg";
 
 export const homeObjOne = {
   id: "about",
@@ -11,7 +13,43 @@ export const homeObjOne = {
     "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
   buttonLable: "Get Started",
   imgStart: false,
-  img: pic,
+  img: infoSectionPic,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLable: "Get Started",
+  imgStart: false,
+  img: infoSectionPic2,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjThree = {
+  id: "services",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited transactions with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLable: "Get Started",
+  imgStart: false,
+  img: infoSectionPic3,
   alt: "Car",
   dark: true,
   primary: true,
